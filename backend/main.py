01251ed3b28e51990d8be39f1aee02dcf23bf594
Alexlee8888 <alexleeyt8888@gmail.com>
2025-02-08 09:07:04 -0500

    frontend can call object detection for backend

--- backend/main.py
+++ backend/main.py
@@ -44,14 +44,6 @@ memory_db = {"fruits": []}
 def classify():
     sort.deepSort()
 
-@app.get("/fruits", response_model=Fruits)
-def get_fruits():
-    return Fruits(fruits=memory_db["fruits"])
-
-@app.post("/fruits", response_model=Fruit)
-def add_fruit(fruit: Fruit):
-    memory_db["fruits"].append(fruit)
-    return fruit
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
